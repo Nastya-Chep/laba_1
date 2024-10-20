81f75fd82fb291d0f26049da1c5f348a5a39e490
Nastya-Chep <anastasiachepkalenko27@gmail.com>
2024-10-20 03:51:02 +0300

    Лабораторная работа 1

---
+++ Лабораторная работа 1/task_3.py
@@ -0,0 +1,9 @@
+players = ["Маша", "Петя", "Саша", "Оля", "Кирилл", "Коля"]
+
+len_ = len(players)
+middle = len_ // 2
+first = players[:middle]
+second = players[middle:]
+
+print(first)
+print(second)
